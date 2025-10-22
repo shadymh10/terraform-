c64a8f6b7675e466e513c377bdba55c7c1ebfd04
shady <shadymhmd2004@gmail.com>
2025-10-23 02:16:01 +0300

    Add security groups for EC2 and Load Balancer

---
+++ sgs.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "project" {
+  name = "project-sg"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "project-sg"
+  }
+}
